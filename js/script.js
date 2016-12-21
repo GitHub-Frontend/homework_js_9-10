5c7ed090624b86f7784d9e5028c775a425682772
Denis IT <git.khalipskyi@gmail.com>
2016-12-21 13:31:27 +0200

    add carousel

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function(){
+
+      $('.owl-carousel').owlCarousel({
+          items: 3,
+          loop:true, //Зацикливаем слайдер
+          margin:5, //Отступ от элемента справа в 50px
+          nav:true, //Отключение навигации
+          autoplay:true, //Автозапуск слайдера
+          smartSpeed:1000, //Время движения слайда
+          autoplayTimeout:4500, //Время смены слайда
+
+      });
+
+});
