28e5ebcaa75260d48f92ea79ec20a2400bfc4efa
Denis IT <git.khalipskyi@gmail.com>
2016-12-30 15:20:44 +0200

    added js for menu

--- js/script.js
+++ js/script.js
@@ -1,22 +1,42 @@
 $(function(){
+    //Menu
+    $("nav a", ".nav-menu").click(function(event) {
+        event.preventDefault();
+    });
+    $(".dropdown").hover(function() {
+        var subMenu =  $(this).children("ul");
+        subMenu.slideToggle(500);
+    });
 
-      $('.owl-carousel').owlCarousel({
-          items: 3,
-          loop:true, //Зацикливаем слайдер
-          margin:5, //Отступ от элемента справа в 50px
-          nav:true, //Отключение навигации
-          autoplay:true, //Автозапуск слайдера
-          smartSpeed:1000, //Время движения слайда
-          autoplayTimeout:4500, //Время смены слайда
+    //Carousel
+    $('.owl-carousel').owlCarousel({
+        // items: 3,
+        loop:true, //Зацикливаем слайдер
+        margin:5, //Отступ от элемента справа в 5px
+        nav:true, //Отключение навигации
+        autoplay:true, //Автозапуск слайдера
+        smartSpeed:1000, //Время движения слайда
+        autoplayTimeout:4500, //Время смены слайда
+        responsive:{ //Адаптация в зависимости от разрешения экрана
+      			0:{
+              items:1
+      			},
+      			720:{
+      				items:2
+      			},
+            1200: {
+              items:3
+            }
+      	}
+    });
 
-      });
+    //Select
+    $('.select-box').SumoSelect({
 
+    });
 
-//TASK 2
-      $('.SlectBox').SumoSelect();
+    //Check-Box
+    $('.check-box, select').styler();
 
-//TASK 3
-      $('.check-box, select').styler();
-
-
+    //End
 });
